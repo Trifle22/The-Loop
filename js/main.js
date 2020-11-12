665fd32c0764d01c5bdbc5b6d89727baef14048f
Trifle22 <xromchenkov99@gmail.com>
2020-11-12 20:32:01 +0300

    +adaptive burger-menu collapse

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuBurger = document.querySelector('.menu-burger');
+const headerDropdown = document.querySelectorAll('.header__dropdown__text');
+const menuDropdown = document.querySelectorAll('.menu__dropdown__text');
+
 
 menuBtn.addEventListener('click', function(event) {
   event.preventDefault();
   menuBtn.classList.toggle('menu-btn-active');
   menuBurger.classList.toggle('menu-burger-active');
+});
+
+
+headerDropdown.forEach(item => {
+  item.addEventListener('click', function() {
+    this.classList.toggle('active');
+  });
+});
+menuDropdown.forEach(item => {
+  item.addEventListener('click', function() {
+    this.classList.toggle('active');
+  });
 });
